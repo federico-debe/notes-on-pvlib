327259309e0026244a49318230247cde2100d549
federico-debe <fede.de.bettin@gmail.com>
2025-10-07 10:54:37 +0200

    started implementing tracking

---
+++ models/pv_custom/dual_axis_tracker_mount.py
@@ -0,0 +1,5 @@
+from pvlib import pvsystem
+
+class DualAxisTrackerMount(pvsystem.AbstractMount):
+    def get_orientation(self, solar_zenith, solar_azimuth):
+        return {'surface_tilt': solar_zenith, 'surface_azimuth': solar_azimuth}
